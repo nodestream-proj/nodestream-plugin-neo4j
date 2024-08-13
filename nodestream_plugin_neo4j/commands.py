66be155b272355f1f58a7626b4b3d2fe8a64f4bb
Henry Lin <henry.lin@neo4j.com>
2024-08-13 16:14:22 -0400

    cli verbage reorder; --wait option for instance creation; Using env vars for aura credentials; keeping cli args for override

--- nodestream_plugin_neo4j/commands.py
+++ nodestream_plugin_neo4j/commands.py
@@ -1,12 +1,18 @@
+import os
 from time import sleep
 
 from cleo.helpers import argument, option
 from neo4j_aura_sdk import AuraClient, models
 from nodestream.cli.commands import NodestreamCommand
 
+AURA_CLIENT_SECRET_ENV = "AURA_CLIENT_SECRET"
+AURA_CLIENT_SECRET_OPT = "aura-client-secret"
+AURA_CLIENT_ID_ENV = "AURA_CLIENT_ID"
+AURA_CLIENT_ID_OPT = "aura-client-id"
+
 
 class CreateAura(NodestreamCommand):
-    name = "create aura"
+    name = "aura create"
     description = "Create neo4j Aura instances via the Aura API"
     arguments = [
         argument("name", "instance name"),
@@ -21,27 +27,34 @@ class CreateAura(NodestreamCommand):
             "cloud provider the instance will be hosted in - possible values: gcp, aws",
         ),
         argument("tenant_id", "tenant_id for the new aura instance"),
-        argument("aura_client_id", "aura api client id"),
     ]
     options = [
-        # making this an option because the secret can start with a dash/hyphen, and
-        # get interpreted as another option
-        option("aura_client_secret", "aura api client secret", flag=False)
+        option(AURA_CLIENT_ID_OPT, description="Aura API client id", flag=False),
+        option(
+            AURA_CLIENT_SECRET_OPT, description="Aura API client secret", flag=False
+        ),
+        option("wait", "w", "Wait for instance status to be running"),
     ]
 
     async def handle_async(self):
-        aura_client_secret = self.option("aura_client_secret")
-        if not aura_client_secret:
-            self.line_error("<error>--aura_client_secret is required</error>")
+        # credentials
+        aura_client_id, aura_client_secret = get_aura_credentials(self)
+        if not aura_client_id:
+            self.line_error(
+                f"<error>{AURA_CLIENT_ID_ENV} variable required or provide --{AURA_CLIENT_ID_OPT}</error>"
+            )
             return
 
-        name = self.argument("name")
-        self.line(f"Creating instance '{name}'...")
-        aura_client_id = self.argument("aura_client_id")
+        if not aura_client_secret:
+            self.line_error(
+                f"<error>{AURA_CLIENT_SECRET_ENV} variable required or provide --{AURA_CLIENT_SECRET_OPT}</error>"
+            )
+            return
+
         async with AuraClient(aura_client_id, aura_client_secret) as client:
             resp = await client.create_instance(
                 models.InstanceRequest(
-                    name=name,
+                    name=self.argument("name"),
                     memory=self.argument("memory"),
                     version="5",
                     region=self.argument("region"),
@@ -53,7 +66,7 @@ class CreateAura(NodestreamCommand):
 
             d = resp.data
             instance_id = d.id
-            self.line("")
+            self.line("\nAURA INSTANCE DETAILS:\n")
             self.line(f"Instance Id = {instance_id}")
             self.line(f"Instance name = {d.name}")
             self.line(f"cloud_provider = {d.cloud_provider}")
@@ -63,28 +76,54 @@ class CreateAura(NodestreamCommand):
             self.line(f"connection_url = {d.connection_url}")
             self.line(f"username = {d.username}")
             self.line(f"password = {d.password}")
-            self.line(
-                f"\nInstance created. To check on status of instance, run:\n   "
-                + f"nodestream status aura {instance_id} $AURA_CLIENT_ID --aura_client_secret=$AURA_CLIENT_SECRET"
-            )
+
+            if self.option("wait"):
+                self.line(f"\nCreating instance...")
+                if resp.data.status != "running":
+                    while resp.data.status != "running":
+                        sleep(10)
+                        resp = await client.instance(instance_id)
+                if resp.data.status != "running":
+                    self.line_error(
+                        f'<error>Error creating instance "{instance_id}" with status "{resp.data.status}"</error>'
+                    )
+                    return
+                self.line("Instance is now running")
+            else:
+                self.line(
+                    f"\nInstance is being created. To check on its status, run:\n   "
+                    + f"nodestream status aura {instance_id}"
+                )
 
 
 class StatusAura(NodestreamCommand):
-    name = "status aura"
+    name = "aura status"
     description = "Check status of Aura instance via the Aura API"
     arguments = [
         argument("instance_id", "instance id"),
-        argument("aura_client_id", "Aura API client id"),
     ]
-    options = [option("aura_client_secret", "Aura API client secret", flag=False)]
+    options = [
+        option(AURA_CLIENT_ID_OPT, description="Aura API client id", flag=False),
+        option(
+            AURA_CLIENT_SECRET_OPT, description="Aura API client secret", flag=False
+        ),
+    ]
 
     async def handle_async(self):
-        aura_client_secret = self.option("aura_client_secret")
-        if not aura_client_secret:
-            self.line_error("<error>--aura_client_secret is required</error>")
+        # credentials
+        aura_client_id, aura_client_secret = get_aura_credentials(self)
+        if not aura_client_id:
+            self.line_error(
+                f"<error>{AURA_CLIENT_ID_ENV} variable required or provide --{AURA_CLIENT_ID_OPT}</error>"
+            )
             return
 
-        aura_client_id = self.argument("aura_client_id")
+        if not aura_client_secret:
+            self.line_error(
+                f"<error>{AURA_CLIENT_SECRET_ENV} variable required or provide --{AURA_CLIENT_SECRET_OPT}</error>"
+            )
+            return
+
         async with AuraClient(aura_client_id, aura_client_secret) as client:
             resp = await client.instance(self.argument("instance_id"))
             self.line(f"Instance Name: {resp.data.name}")
@@ -92,26 +131,43 @@ class StatusAura(NodestreamCommand):
 
 
 class RemoveAura(NodestreamCommand):
-    name = "remove aura"
+    name = "aura remove"
     description = "Remove Aura instance by instance Id"
     arguments = [
         argument("instance_id", "instance id"),
-        argument("aura_client_id", "aura api client id"),
     ]
     options = [
-        # making this an option because the secret can being with a dash/hyphen, which
-        # gets interpreted as another option
-        option("aura_client_secret", "aura api client secret", flag=False)
+        option(AURA_CLIENT_ID_OPT, description="Aura API client id", flag=False),
+        option(
+            AURA_CLIENT_SECRET_OPT, description="Aura API client secret", flag=False
+        ),
     ]
 
     async def handle_async(self):
-        aura_client_secret = self.option("aura_client_secret")
-        if not aura_client_secret:
-            self.line_error("<error>--aura_client_secret is required</error>")
+        # credentials
+        aura_client_id, aura_client_secret = get_aura_credentials(self)
+        if not aura_client_id:
+            self.line_error(
+                f"<error>{AURA_CLIENT_ID_ENV} variable required or provide --{AURA_CLIENT_ID_OPT}</error>"
+            )
             return
 
-        async with AuraClient(
-            self.argument("aura_client_id"), aura_client_secret
-        ) as client:
+        if not aura_client_secret:
+            self.line_error(
+                f"<error>{AURA_CLIENT_SECRET_ENV} variable required or provide --{AURA_CLIENT_SECRET_OPT}</error>"
+            )
+            return
+
+        async with AuraClient(aura_client_id, aura_client_secret) as client:
             resp = await client.delete_instance(self.argument("instance_id"))
             self.line(f"{resp}")
+
+
+def get_aura_credentials(ctx: NodestreamCommand):
+    client_id = ctx.option(AURA_CLIENT_ID_OPT)
+    if not client_id:
+        client_id = os.environ.get(AURA_CLIENT_ID_ENV, "")
+    client_secret = ctx.option(AURA_CLIENT_SECRET_OPT)
+    if not client_secret:
+        client_secret = os.environ.get(AURA_CLIENT_SECRET_ENV, "")
+    return client_id, client_secret
